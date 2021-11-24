5fda5c73e87251175c9cf3427a927486865cf8d9
UncleMaximilian <jj720462@gmail.com>
2021-11-24 14:54:01 -0600

    first commit

---
+++ Taupro2.bat
@@ -0,0 +1,39 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFC5aTwiHAE+1BaAR7ebv/NawrV8WaOA+bIzT5r2NL+4A1lHlYIQ4xTRTm8Rs
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC5aTwiHAE+1BaAR7ebv/NawrV8WaOA+bIzT5r2NL+4A1kTxZYQihDRfgM5s
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+title Blackgate Cyber LLC
+cls
+@echo off
+break>DUMP_PRC.2
+FOR /F "tokens=* delims=" %%x in (TAU_PRC.2) DO echo %%x >> DUMP_PRC.2 
+exit
